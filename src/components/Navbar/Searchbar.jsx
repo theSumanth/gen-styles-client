02ec2c856d24c92d37f19d1214591bf1023b52eb
Sumanth Bojugu <bojugusumanthkumar@gmail.com>
2024-11-25 14:15:42 +0530

    added ui for image search and to send it to api

--- src/components/Navbar/Searchbar.jsx
+++ src/components/Navbar/Searchbar.jsx
@@ -1,15 +1,26 @@
 import { motion } from "framer-motion";
-import { useContext, useState } from "react";
-
-import aiTextSearchImg from "../../assets/search.png";
-import aiVoiceSearchImg from "../../assets/voice.png";
-import aiImageSearchImg from "../../assets/image-search.png";
-import { ChevronDown } from "lucide-react";
-import { SearchContext } from "../../store/SearchContextProvider";
+import { useContext, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import AiSearchSvg from "../../assets/AiSearchSvg";
+import AiVoiceSearchSvg from "../../assets/AiVoiceSearchSvg";
+import AiImageSearchSvg from "../../assets/AiImageSearchSvg";
+
+import { toast } from "sonner";
+import { quantum } from "ldrs";
+
+quantum.register();
+
+import { ChevronDown, CircleCheckBig, Sparkle, Upload } from "lucide-react";
+import { SearchContext } from "../../store/SearchContextProvider";
+import Modal from "../UI/Modal";
+import CustomSquareButton from "../UI/CustomSquareButton";
+import Button from "../UI/Button";
 
 const Searchbar = () => {
   const [isSearchTypeClicked, setIsSearchTypeClicked] = useState(false);
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false);
+  const imageInputRef = useRef();
   const navigate = useNavigate();
 
   const {
@@ -17,34 +28,61 @@ const Searchbar = () => {
     getSearchText,
     selectedSearchType,
     setSelectedSearchType,
+    imageFile,
+    setImageFile,
     fetchAISearch,
   } = useContext(SearchContext);
 
-  const searchIconimages = {
-    "Text Search": aiTextSearchImg,
-    "Voice Search": aiVoiceSearchImg,
-    "Image Search": aiImageSearchImg,
+  const SearchIcons = {
+    "Text Search": AiSearchSvg,
+    "Voice Search": AiVoiceSearchSvg,
+    "Image Search": AiImageSearchSvg,
   };
+  const SelectedSearchIcon = SearchIcons[selectedSearchType];
 
   function handleSearchTypeClick() {
     setIsSearchTypeClicked((prev) => !prev);
   }
 
-  function textSearch() {
-    console.log("in text search function");
+  function textAISearch() {
+    if (getSearchText().trim() === "") {
+      toast.warning("Please enter a text to search");
+      return;
+    }
     navigate("/search");
-    fetchAISearch({ queryText: getSearchText() });
+    fetchAISearch({ selectedSearchType });
+  }
+
+  function imageSearchClick() {
+    setIsImageModalOpen(true);
+  }
+
+  function imageAISearch() {
+    setIsImageModalOpen(false);
+    navigate("/search");
+    fetchAISearch({ selectedSearchType });
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      // console.log(file);
+      setImageFile(file);
+    }
+  }
+
+  function handleUploadImageClick() {
+    imageInputRef.current.click();
   }
 
   const handleSearchClick = () => {
-    console.log(getSearchText());
     switch (selectedSearchType) {
       case "Text Search":
-        return textSearch();
+        return textAISearch();
       case "Voice Search":
         return () => {};
       case "Image Search":
-        return () => {};
+        return imageSearchClick();
       default:
         console.error("Invalid search type");
     }
@@ -87,22 +125,62 @@ const Searchbar = () => {
         onClick={handleSearchClick}
         className="absolute right-2 h-10 rounded-full bg-customBlue px-2 border-2 border-[#746eea] text-white font-medium flex items-center justify-center"
       >
-        <motion.img
-          src={searchIconimages[selectedSearchType]}
-          alt="ai sparkle icon"
-          initial={{ scale: 1.4 }}
-          animate={{ scale: 1, transition: { duration: 2 } }}
-          whileHover={{
-            scale: 1.3,
-            transition: { duration: 0.2, ease: "easeInOut" },
-          }}
-          whileTap={{
-            scale: 0.99,
-            transition: { duration: 0.4, ease: "easeInOut" },
-          }}
-          className="w-5 h-5 filter invert brightness-0"
-        />
+        <SelectedSearchIcon />
       </motion.button>
+
+      {selectedSearchType === "Image Search" && isImageModalOpen && (
+        <Modal
+          open={isImageModalOpen}
+          onClose={() => setIsImageModalOpen(false)}
+        >
+          <input
+            ref={imageInputRef}
+            type="file"
+            accept="image/png, image/jpeg, image/jpg"
+            onChange={handleImageChange}
+            className="hidden"
+          />
+          <div className="relative flex flex-col gap-1 w-80 not-mobile-view:w-96 h-[25rem] items-center justify-center bg-white rounded-md p-4">
+            <div className="absolute flex flex-col items-center justify-center left-1/2 -translate-x-1/2 top-6">
+              <h4 className="text-base font-bold text-neutral-700">
+                Upload an Image ✨
+              </h4>
+              <span className="text-neutral-500 text-xs whitespace-nowrap">
+                File should be JPG, PNG, JPEG
+              </span>
+            </div>
+
+            <div className="relative flex flex-col justify-center items-center gap-2 w-full h-64 mt-14 p-4 border-2 bg-customBlue bg-opacity-5 border-dashed rounded-xl border-customBlue">
+              <Upload size={42} className="text-customBlue" />
+              <span className="text-xs text-neutral-500">
+                Upload your file here
+              </span>
+              <Button
+                onClick={handleUploadImageClick}
+                className={
+                  "md:block text-white bg-customBlue text-base whitespace-nowrap rounded-full"
+                }
+              >
+                Browse Files
+              </Button>
+
+              {imageFile && (
+                <p className="absolute flex items-center justify-center gap-1 left-1/2 -translate-x-1/2 bottom-7 text-neutral-700 font-semibold text-xs">
+                  <CircleCheckBig size={12} />
+                  <span>{imageFile.name}</span>
+                </p>
+              )}
+            </div>
+
+            <CustomSquareButton
+              onClick={imageAISearch}
+              LucideIcon={Sparkle}
+              label={"Search Image"}
+              className={"w-full mt-5 flex items-center justify-center"}
+            />
+          </div>
+        </Modal>
+      )}
     </motion.div>
   );
 };
